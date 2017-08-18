8f69f6a8c19d82bd7b422412cbf01de94fe82a85
HEB <31109925+SofttekPOC@users.noreply.github.com>
2017-08-18 17:55:53 -0500

    Update handlers.js

--- modules/handlers.js
+++ modules/handlers.js
@@ -5,7 +5,7 @@ let salesforce = require('./salesforce'),
     formatter = require('./formatter');
 
 exports.searchHouse = (sender) => {
-    messenger.send({text: `OK, looking for houses for sale around you...`}, sender);
+    messenger.send({text: `No estes chingando...`}, sender);
     salesforce.findProperties().then(properties => {
         messenger.send(formatter.formatProperties(properties), sender);
     });
