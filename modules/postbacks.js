715bda363d488c9b35e9558a80f5e3a1ace3ef29
HEB <31109925+SofttekPOC@users.noreply.github.com>
2017-08-21 14:45:05 -0500

    Update postbacks.js

--- modules/postbacks.js
+++ modules/postbacks.js
@@ -30,3 +30,15 @@ exports.contact_me = (sender, values) => {
 
 };
 
+exports.quiz_1 = (sender, values) => { 
+    let branchId = values[1];
+	let answer_1 = values[2];
+	
+	messenger.getUserInfo(sender).then(response => { 
+		salesforce.createQuiz(branchId, answer, response.first_name + " " + response.first_name, sender).then(() => { 			
+			messenger.send({text: `Thanks for your answer.`}, sender); 
+		}); 
+	});
+
+}; 
+
