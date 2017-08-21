c66e13e6688d207b448e28d76301349a8f0e6f6b
HEB <31109925+SofttekPOC@users.noreply.github.com>
2017-08-21 18:15:48 -0500

    Update formatter.js

--- modules/formatter.js
+++ modules/formatter.js
@@ -171,7 +171,7 @@ exports.formatSucursal = sucursal => {
      };
 };
 
-exports.question_1 = broker => { 
+exports.question_1 = sucursal => { 
     	let elements = []; 
 	elements.push({ 
 		title: "1st Question",
@@ -180,17 +180,17 @@ exports.question_1 = broker => {
 			{ 
 				"type": "postback", 
 				 "title": "Good", 
-				 "payload": "quiz_1," + broker.get("Id") + "," + "Good" 
+				 "payload": "quiz_1"
 			},
 			{ 
 				"type": "postback", 
 				 "title": "Regular", 
-				 "payload": "quiz_1," + broker.get("Id") + "," + "Regular" 
+				 "payload": "quiz_1"
 				},
 			{ 
 				"type": "postback", 
 				 "title": "Bad", 
-				 "payload": "quiz_1," + broker.get("Id") + "," + "Bad" 
+				 "payload": "quiz_1"
 			}
 		] 
     	}); 
