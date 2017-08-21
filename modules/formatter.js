a1dbcbd6411ff7cb08cdc1aee02417418ae5650b
HEB <31109925+SofttekPOC@users.noreply.github.com>
2017-08-21 16:41:57 -0500

    Update formatter.js

--- modules/formatter.js
+++ modules/formatter.js
@@ -148,7 +148,7 @@ exports.formatSucursal = sucursal => {
                 subtitle: `${sucursal.get("BillingCity")}`,
                 "buttons":[{
                     "type":"web_url",
-                    "url":"https://www.google.com.mx/maps/place/H-E-B+Contry/@25.6261449,-100.274875,15z/data=!4m5!3m4!1s0x0:0xfacdabf372954c90!8m2!3d25.6261449!4d-100.274875",
+                    "url": sucursal.HEB_Location__c,
                     "title":"View Ubication",
                     "webview_height_ratio": "compact"
                 },
