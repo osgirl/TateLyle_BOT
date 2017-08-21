0cdee0d310178f153d702183281ce7a279dab857
HEB <31109925+SofttekPOC@users.noreply.github.com>
2017-08-21 18:05:39 -0500

    Update formatter.js

--- modules/formatter.js
+++ modules/formatter.js
@@ -180,17 +180,17 @@ exports.question_1 = sucursal => {
 			{ 
 				"type": "postback", 
 				 "title": "Good", 
-				 "payload": "quiz_1," + sucursal.getId(), + "Good" 
+				 "payload": "quiz_1," + sucursal.get("Id") + "," + "Good" 
 			},
 			{ 
 				"type": "postback", 
 				 "title": "Regular", 
-				 "payload": "quiz_1," + sucursal.getId(), + "Regular" 
+				 "payload": "quiz_1," + sucursal.get("Id") + "," + "Regular" 
 				},
 			{ 
 				"type": "postback", 
 				 "title": "Bad", 
-				 "payload": "quiz_1," + sucursal.getId(), + "Bad" 
+				 "payload": "quiz_1," + sucursal.get("Id") + "," + "Bad" 
 			}
 		] 
     	}); 
