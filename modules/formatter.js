2853c1d81049d54c80ac9114d172b3c5f76cb16b
HEB <31109925+SofttekPOC@users.noreply.github.com>
2017-08-21 11:22:28 -0500

    Update formatter.js

--- modules/formatter.js
+++ modules/formatter.js
@@ -147,7 +147,6 @@ exports.formatSucursal = sucursal => {
                 title: sucursal.get("Name"),                
                 subtitle: `${sucursal.get("BillingCity")}`,
                 location: `https://www.google.com.mx/maps/place/H-E-B+Contry/@25.6261449,-100.274875,15z/data=!4m5!3m4!1s0x0:0xfacdabf372954c90!8m2!3d25.6261449!4d-100.274875`,
-                "image_url": sucursal.get("Picture__c"),
                 "buttons": [
                     {
                         "type": "postback",
