445c7d23ad3b472d3b3ef3b3d190b0c1f524bbd1
HEB <31109925+SofttekPOC@users.noreply.github.com>
2017-08-23 17:56:00 -0500

    Update formatter.js

--- modules/formatter.js
+++ modules/formatter.js
@@ -151,7 +151,7 @@ exports.formatSucursal = sucursal => {
 		"image_url": sucursal.get("HEB_Front_Picture__c"),
                 "buttons":[{
                     "type":"web_url",
-                    "url": `${sucursal.get("HEB_Location__c")}`,
+                    "url": "www.google.com",
                     "title":"View Ubication",
                     "webview_height_ratio": "compact"
                 },
