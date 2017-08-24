a398fc8d74d4b8bbd44caa83409c4f9902079a17
HEB <31109925+SofttekPOC@users.noreply.github.com>
2017-08-24 10:41:00 -0500

    Update formatter.js

--- modules/formatter.js
+++ modules/formatter.js
@@ -146,6 +146,7 @@ exports.formatSucursal = sucursal => {
 	    elements.push({                
                 title: sucursal.get("Name"),                
                 subtitle: `${sucursal.get("BillingCity")}, ${sucursal.get("BillingStreet")}`,
+	    	"text": sucursal.get("Phone"),
 		"image_url": sucursal.get("HEB_Front_Picture__c"),
                 "buttons":[{
                     "type":"web_url",
