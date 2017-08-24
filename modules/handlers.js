1a8a881e432a083b12214f9e2ab42e79710cc7f3
HEB <31109925+SofttekPOC@users.noreply.github.com>
2017-08-24 12:30:56 -0500

    Update handlers.js

--- modules/handlers.js
+++ modules/handlers.js
@@ -18,55 +18,6 @@ exports.searchProducts_Offers = (sender) => {       
     });
 };
 
-exports.searchHouse = (sender) => {
-    messenger.send({text: `Search Houses...`}, sender);
-    salesforce.findProperties().then(properties => {
-        messenger.send(formatter.formatProperties(properties), sender);
-    });
-};
-
-exports.searchHouse_City = (sender, values) => {
-    messenger.send({text: `OK, Searching Houses in ${values[1]}`}, sender);
-    salesforce.findProperties({city: values[1]}).then(properties => {
-        messenger.send(formatter.formatProperties(properties), sender);
-    });
-};
-
-exports.searchHouse_Bedrooms_City_Range = (sender, values) => {
-    messenger.send({text: `OK, looking for ${values[1]} bedrooms in ${values[2]} between ${values[3]} and ${values[4]}`}, sender);
-    salesforce.findProperties({bedrooms: values[1], city: values[2]}).then(properties => {
-        messenger.send(formatter.formatProperties(properties), sender);
-    });
-};
-
-exports.searchHouse_Bedrooms_City = (sender, values) => {
-    messenger.send({text: `OK, looking for ${values[1]} bedroom houses in ${values[2]}`}, sender);
-    salesforce.findProperties({bedrooms: values[1], city: values[2]}).then(properties => {
-        messenger.send(formatter.formatProperties(properties), sender);
-    });
-};
-
-exports.searchHouse_Bedrooms = (sender, values) => {
-    messenger.send({text: `OK, looking for ${values[1]} bedrooms`}, sender);
-    salesforce.findProperties({bedrooms: values[1]}).then(properties => {
-        messenger.send(formatter.formatProperties(properties), sender);
-    });
-};
-
-exports.searchHouse_Range = (sender, values) => {
-    messenger.send({text: `OK, looking for houses between ${values[1]} and ${values[2]}`}, sender);
-    salesforce.findProperties({priceMin: values[1], priceMax: values[2]}).then(properties => {
-        messenger.send(formatter.formatProperties(properties), sender);
-    });
-};
-
-exports.priceChanges = (sender, values) => {
-    messenger.send({text: `OK, looking for recent price changes...`}, sender);
-    salesforce.findPriceChanges().then(priceChanges => {
-        messenger.send(formatter.formatPriceChanges(priceChanges), sender);
-    });
-};
-
 exports.hi = (sender) => {
     messenger.getUserInfo(sender).then(response => {
         messenger.send({text: `Hello, ${response.first_name}!`}, sender);
@@ -74,19 +25,5 @@ exports.hi = (sender) => {
 };
 
 exports.help = (sender) => {
-    messenger.send({text: `You can ask me questions like "Find houses in Boston", "3 bedrooms in Boston", "3 bedrooms in Boston between 500000 and 750000", "show me price changes"`}, sender);
-};
-
-exports.hola = (sender) => {   
-    messenger.getUserInfo(sender).then(response => {        
-        messenger.send({text: `Hola, ${response.first_name}!`}, sender);    
-    });
-};
-
-exports.queja = (sender, values) => { 
-    messenger.getUserInfo(sender).then(response => { 
-        salesforce.createCase2(response.first_name + " " + response.first_name, sender).then(() => { 
-            messenger.send({text: `Gracias, ${response.first_name}. Una queja ha sido creada.`}, sender); 
-        }); 
-    }); 
+    messenger.send({text: `You can ask me questions like "H-E-B in Monterrey", "Stores in Monterrey"`}, sender);
 };
