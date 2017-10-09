15dd8a3ff4cfc345e01dc265899876ed374f473f
HEB <31109925+SofttekPOC@users.noreply.github.com>
2017-10-09 12:05:09 -0500

    Update handlers.js

--- modules/handlers.js
+++ modules/handlers.js
@@ -40,6 +40,6 @@ exports.help = (sender) => {
 
 exports.answerSurvey = (sender, values) => {        
     messenger.send({text: `Survey for Ticket ${values[1]}`}, sender);   
-    salesforce.TAL_Question_1();
+    salesforce.start_quiz();
 };//End Tate And Lyle Code ************************** End Tate And Lyle Code*/
 
