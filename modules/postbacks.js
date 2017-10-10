b7d959f4e1c71af828ca8f96e6b006d8cb140342
HEB <31109925+SofttekPOC@users.noreply.github.com>
2017-10-10 12:05:10 -0500

    Update postbacks.js

--- modules/postbacks.js
+++ modules/postbacks.js
@@ -88,7 +88,6 @@ exports.quiz_5 = (sender, values) => {
 };
 
 exports.end_quiz = (sender, values) => { 
-    global_variable[5] = values[1];
     messenger.getUserInfo(sender).then(response => { 
         salesforce.createSurvey(global_variable, response.first_name + " " + response.last_name, sender).then(() => { 			
 	    messenger.send({text: `Thank you for answering our survey. Your comments are very much appreciated.`}, sender); 
