95457dcd15420d70309a05ac22754575f2fe36e0
HEB <31109925+SofttekPOC@users.noreply.github.com>
2017-10-10 10:02:06 -0500

    Update formatter.js

--- modules/formatter.js
+++ modules/formatter.js
@@ -156,7 +156,7 @@ exports.TAL_Question_1 = sucursal => {
     let elements = []; 
     elements.push({ 
         title: "How satisfied do you feel with the technical knowledge of those who fulfilled your request?",
-        subtitle: "1=Lowest, 5=Highest",
+        subtitle: "1=Lowest, 3=Highest",
         "buttons": [ 
             { 
                 "type": "postback", 
@@ -172,11 +172,142 @@ exports.TAL_Question_1 = sucursal => {
                 "type": "postback", 
                 "title": "3", 
                 "payload": "quiz_1," + "3"
+            }
+        ]  
+    }); 
+    return { 
+        "attachment": { 
+            "type": "template", 
+            "payload": { 
+                "template_type": "generic", 
+                "elements": elements 
+            } 
+        } 
+    }; 
+};
+
+exports.TAL_Question_2 = sucursal => { 
+    let elements = []; 
+    elements.push({ 
+        title: "Overall satisfaction with support provided in relation to your request?",
+        subtitle: "1=Lowest, 3=Highest",
+        "buttons": [ 
+            { 
+                "type": "postback", 
+                "title": "1", 
+                "payload": "quiz_2," + "1"
             },
 	    { 
                 "type": "postback", 
-                "title": "4", 
-                "payload": "quiz_1," + "4"
+                "title": "2", 
+                "payload": "quiz_2," + "2"
+            },
+	    { 
+                "type": "postback", 
+                "title": "3", 
+                "payload": "quiz_2," + "3"
+            }
+        ]  
+    }); 
+    return { 
+        "attachment": { 
+            "type": "template", 
+            "payload": { 
+                "template_type": "generic", 
+                "elements": elements 
+            } 
+        } 
+    }; 
+};
+
+exports.TAL_Question_3 = sucursal => { 
+    let elements = []; 
+    elements.push({ 
+        title: "How satisfied do you feel with how quickly your request was resolved?",
+        subtitle: "1=Lowest, 3=Highest",
+        "buttons": [ 
+            { 
+                "type": "postback", 
+                "title": "1", 
+                "payload": "quiz_3," + "1"
+            },
+	    { 
+                "type": "postback", 
+                "title": "2", 
+                "payload": "quiz_3," + "2"
+            },
+	    { 
+                "type": "postback", 
+                "title": "3", 
+                "payload": "quiz_3," + "3"
+            }
+        ]  
+    }); 
+    return { 
+        "attachment": { 
+            "type": "template", 
+            "payload": { 
+                "template_type": "generic", 
+                "elements": elements 
+            } 
+        } 
+    }; 
+};
+
+exports.TAL_Question_4 = sucursal => { 
+    let elements = []; 
+    elements.push({ 
+        title: "Do you feel your ticket was given the urgency and priority that was correct?",
+        subtitle: "1=Lowest, 3=Highest",
+        "buttons": [ 
+            { 
+                "type": "postback", 
+                "title": "1", 
+                "payload": "quiz_4," + "1"
+            },
+	    { 
+                "type": "postback", 
+                "title": "2", 
+                "payload": "quiz_4," + "2"
+            },
+	    { 
+                "type": "postback", 
+                "title": "3", 
+                "payload": "quiz_4," + "3"
+            }
+        ]  
+    }); 
+    return { 
+        "attachment": { 
+            "type": "template", 
+            "payload": { 
+                "template_type": "generic", 
+                "elements": elements 
+            } 
+        } 
+    }; 
+};
+
+exports.TAL_Question_5 = sucursal => { 
+    let elements = []; 
+    elements.push({ 
+        title: "Please rate overall courtesy of the staff.",
+        subtitle: "1=Lowest, 3=Highest",
+        "buttons": [ 
+            { 
+                "type": "postback", 
+                "title": "1", 
+                "payload": "quiz_5," + "1"
+            },
+	    { 
+                "type": "postback", 
+                "title": "2", 
+                "payload": "quiz_5," + "2"
+            },
+	    { 
+                "type": "postback", 
+                "title": "3", 
+                "payload": "quiz_5," + "3"
             }
         ]  
     }); 
