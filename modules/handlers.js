9ef6b83758bae05692868605a02a7419944d6138
HEB <31109925+SofttekPOC@users.noreply.github.com>
2017-10-17 11:44:15 -0500

    Update handlers.js

--- modules/handlers.js
+++ modules/handlers.js
@@ -28,7 +28,7 @@ exports.addCommentsText = (sender, values) => { 
 
 exports.saveValues = (sender, values) => { 
     messenger.send({text: `Value ${values[1]}`}, sender);  
-    if(postbacks.questionNum == 1){
+    if(postbacks.questionNum() == 1){
         messenger.send({text: `Question 1`}, sender);  
     } else if(postbacks.questionNum == 2){
     }
