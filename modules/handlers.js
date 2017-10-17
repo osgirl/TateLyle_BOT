dd721732bec1d7c2916b70f1cfba0e5bb0054a73
HEB <31109925+SofttekPOC@users.noreply.github.com>
2017-10-17 12:14:26 -0500

    Update handlers.js

--- modules/handlers.js
+++ modules/handlers.js
@@ -20,11 +20,11 @@ exports.answerSurvey = (sender, values) => {       
     messenger.send(formatter.start_Question(), sender); 
 };
 
-exports.addCommentsText = (sender, values) => { 
+/*exports.addCommentsText = (sender, values) => { 
     messenger.getUserInfo(sender).then(response => {
         postbacks.end_quiz(sender, values);
     });
-};
+};*/
 
 exports.saveValues = (sender, values) => { 
     messenger.send({text: `Value ${values[1]}`}, sender); 
