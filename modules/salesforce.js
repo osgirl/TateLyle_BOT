0b23e8c457203a1240659cfa43fac56aea58df8a
HEB <31109925+SofttekPOC@users.noreply.github.com>
2017-10-19 23:40:13 -0500

    Update salesforce.js

--- modules/salesforce.js
+++ modules/salesforce.js
@@ -55,7 +55,7 @@ let findLocations = (params) => {
         //if(params.id) parts.push(`id='${params.id}'`);
         if(params.city) parts.push(`Billingcity='${params.city}'`);
         if(parts.length>0){
-            where = parts.join(' AND ');
+            where = "WHERE " + parts.join(' AND ');
         }
     } 
     return new Promise((resolve, reject) => {        
@@ -67,9 +67,9 @@ let findLocations = (params) => {
                 HEB_Front_Picture__c,
                 HEB_Location__c,
                 HEB_City__c
-                FROM Account  
-		WHERE Industry = 'Other'
-		${where};  	       
+                FROM Account                
+                ${where}
+                LIMIT 5`;       
         org.query({query: q}, (err, resp) => {            
             if (err) {               
                 reject("An error as occurred");            
